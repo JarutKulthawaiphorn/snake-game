eeed68833f205c41142fffc0258c234de5763b5f
Jarut Kulthawaiphorn <jarut.k@echoplus.io>
2026-03-14 22:01:20 +0700

    added namespace for GridMapManager class

--- Assets/Scripts/GridMap/GridMapManager.cs
+++ Assets/Scripts/GridMap/GridMapManager.cs
@@ -1,45 +1,48 @@
 using UnityEngine;
 
-public class GridMapManager : MonoBehaviour
+namespace SnakeGame.Manager
 {
-    #region  Serialized Fields
-    [SerializeField] private Transform _gridMapContainer;
-
-    [Header("Tile Settings")]
-    [SerializeField] private SpriteRenderer _tilePrefab;
-    [SerializeField] private Color _whiteTileColor = Color.white;
-    [SerializeField] private Color _blackTileColor = Color.black;
-
-    [Header("Grid Map Settings")]
-    [SerializeField] private int _width;
-    [SerializeField] private int _height;
-    #endregion
-
-    #region  Public Methods
-    public Vector2 GenerateGridMap()
+    public class GridMapManager : MonoBehaviour
     {
-        for (int x = 0; x < _width; x++)
+        #region  Serialized Fields
+        [SerializeField] private Transform _gridMapContainer;
+
+        [Header("Tile Settings")]
+        [SerializeField] private SpriteRenderer _tilePrefab;
+        [SerializeField] private Color _whiteTileColor = Color.white;
+        [SerializeField] private Color _blackTileColor = Color.black;
+
+        [Header("Grid Map Settings")]
+        [SerializeField] private int _width;
+        [SerializeField] private int _height;
+        #endregion
+
+        #region  Public Methods
+        public Vector2 GenerateGridMap()
         {
-            for (int y = 0; y < _height; y++)
+            for (int x = 0; x < _width; x++)
             {
-                SpriteRenderer tile = Instantiate(_tilePrefab, new Vector3(x, y, 0), Quaternion.identity, _gridMapContainer);
-                tile.color = (x + y) % 2 == 0 ? _whiteTileColor : _blackTileColor;
-                tile.name = $"GridTile {x}, {y}";
+                for (int y = 0; y < _height; y++)
+                {
+                    SpriteRenderer tile = Instantiate(_tilePrefab, new Vector3(x, y, 0), Quaternion.identity, _gridMapContainer);
+                    tile.color = (x + y) % 2 == 0 ? _whiteTileColor : _blackTileColor;
+                    tile.name = $"GridTile {x}, {y}";
+                }
             }
+
+            CenterGridMap();
+            return new Vector2(_width, _height);
         }
+        #endregion
 
-        CenterGridMap();
-        return new Vector2(_width, _height);
+        #region  Private Methods
+        private void CenterGridMap()
+        {
+            //Center the grid map in the scene
+            float offsetX = (_width - 1) / 2f;
+            float offsetY = (_height - 1) / 2f;
+            _gridMapContainer.position = new Vector3(-offsetX, -offsetY, 0);
+        }
+        #endregion
     }
-    #endregion
-
-    #region  Private Methods
-    private void CenterGridMap()
-    {
-        //Center the grid map in the scene
-        float offsetX = (_width - 1) / 2f;
-        float offsetY = (_height - 1) / 2f;
-        _gridMapContainer.position = new Vector3(-offsetX, -offsetY, 0);
-    }
-    #endregion
 }
